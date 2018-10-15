2b683c545f7d22520942286b831ba4ea412d3cdd
tommyyeh0505 <tommyyeh0505@hotmail.com>
2018-10-15 12:28:48 -0700

    added javasource

---
+++ JavaSource/main.java
@@ -0,0 +1,17 @@
+
+/**
+ * main.
+ *
+ * @author Tommy Yeh (Jen-Hao) A01025451 
+ * @version 2017
+ */
+public class main {
+
+    /**
+     * Constructs an objecct of type main.
+     */
+    public main() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
