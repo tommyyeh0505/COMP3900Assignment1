c9317d5cb1c2affc8d0387643b5cbb9e073f01ee
tommyyeh0505 <tommyyeh0505@hotmail.com>
2018-10-15 17:28:41 -0700

    Added Timesheet edit

--- JavaSource/TimesheetManager.java
+++ JavaSource/TimesheetManager.java
@@ -11,6 +11,7 @@ import javax.inject.Named;
 import ca.bcit.infosys.employee.Employee;
 import ca.bcit.infosys.timesheet.Timesheet;
 import ca.bcit.infosys.timesheet.TimesheetCollection;
+import ca.bcit.infosys.timesheet.TimesheetRow;
 
 
 /**
@@ -24,103 +25,107 @@ import ca.bcit.infosys.timesheet.TimesheetCollection;
 public class TimesheetManager implements Serializable, TimesheetCollection {
 
     private Employee testEmployee;
-    
+
     private ArrayList<Employee> employees;
     private ArrayList<Timesheet> timesheets;
-        private Timesheet currentTimesheet;
-        
-        /**
-         * Returns the {bare_field_name} for this TimesheetManager.
-         * @return the currentTimesheet
-         */
-        public Timesheet getCurrentTimesheet() {
-            return currentTimesheet;
-        }
+    private Timesheet currentTimesheet;
 
-        /**
-         * Sets the currentTimesheet for this TimesheetManager
-         * @param currentTimesheet the currentTimesheet to set
-         */
-        public void setCurrentTimesheet(Timesheet currentTimesheet) {
-            this.currentTimesheet = currentTimesheet;
-        }
+    /**
+     * Returns the {bare_field_name} for this TimesheetManager.
+     * @return the currentTimesheet
+     */
+    public Timesheet getCurrentTimesheet() {
+        return currentTimesheet;
+    }
 
-        /**
-         * Sets the timesheets for this TimesheetManager
-         * @param timesheets the timesheets to set
-         */
-        public void setTimesheets(ArrayList<Timesheet> timesheets) {
-            this.timesheets = timesheets;
-        }
+    /**
+     * Sets the currentTimesheet for this TimesheetManager
+     * @param currentTimesheet the currentTimesheet to set
+     */
+    public void setCurrentTimesheet(Timesheet currentTimesheet) {
+        this.currentTimesheet = currentTimesheet;
+    }
 
-        public TimesheetManager () {
-            timesheets = new ArrayList<Timesheet>();
-           testEmployee = new Employee();
-           testEmployee.setName("Tommy");
-        }
+    /**
+     * Sets the timesheets for this TimesheetManager
+     * @param timesheets the timesheets to set
+     */
+    public void setTimesheets(ArrayList<Timesheet> timesheets) {
+        this.timesheets = timesheets;
+    }
 
-        /**
-         * @see ca.bcit.infosys.timesheet.TimesheetCollection#getTimesheets()
-         * @return
-         */
-        @Override
-        public List<Timesheet> getTimesheets() {
-            return timesheets;
-        }
+    public TimesheetManager () {
+        timesheets = new ArrayList<Timesheet>();
+        testEmployee = new Employee();
+        testEmployee.setName("Tommy");
+    }
 
-        /**
-         * @see ca.bcit.infosys.timesheet.TimesheetCollection#getTimesheets(ca.bcit.infosys.employee.Employee)
-         * @param e
-         * @return
-         */
-        @Override
-        public List<Timesheet> getTimesheets(Employee e) {
-            ArrayList<Timesheet> temp = new ArrayList<Timesheet>();
-            for(Timesheet t: timesheets) {
-                if(t.getEmployee().equals(e)) {
-                    temp.add(t);
-                }
+    /**
+     * @see ca.bcit.infosys.timesheet.TimesheetCollection#getTimesheets()
+     * @return
+     */
+    @Override
+    public List<Timesheet> getTimesheets() {
+        return timesheets;
+    }
+
+    /**
+     * @see ca.bcit.infosys.timesheet.TimesheetCollection#getTimesheets(ca.bcit.infosys.employee.Employee)
+     * @param e
+     * @return
+     */
+    @Override
+    public List<Timesheet> getTimesheets(Employee e) {
+        ArrayList<Timesheet> temp = new ArrayList<Timesheet>();
+        for(Timesheet t: timesheets) {
+            if(t.getEmployee().equals(e)) {
+                temp.add(t);
             }
-            return temp;
         }
+        return temp;
+    }
 
-        /**
-         * @see ca.bcit.infosys.timesheet.TimesheetCollection#getCurrentTimesheet(ca.bcit.infosys.employee.Employee)
-         * @param e
-         * @return
-         */
-        @Override
-        public Timesheet getCurrentTimesheet(Employee e) {
-            Timesheet temp = null;
+    /**
+     * @see ca.bcit.infosys.timesheet.TimesheetCollection#getCurrentTimesheet(ca.bcit.infosys.employee.Employee)
+     * @param e
+     * @return
+     */
+    @Override
+    public Timesheet getCurrentTimesheet(Employee e) {
+        Timesheet temp = null;
 
-            Calendar c = new GregorianCalendar();
-            int currentDay = c.get(Calendar.DAY_OF_WEEK);
-            int leftDays = Calendar.FRIDAY - currentDay;
-            c.add(Calendar.DATE, leftDays);
-            Date endWeek = c.getTime();
-            
-            for(Timesheet t: timesheets) {
-                if(t.getEndWeek().equals(endWeek)) {
-                    temp = t;
-                }
+        Calendar c = new GregorianCalendar();
+        int currentDay = c.get(Calendar.DAY_OF_WEEK);
+        int leftDays = Calendar.FRIDAY - currentDay;
+        c.add(Calendar.DATE, leftDays);
+        Date endWeek = c.getTime();
+
+        for(Timesheet t: timesheets) {
+            if(t.getEndWeek().equals(endWeek)) {
+                temp = t;
             }
-            
-            return temp;
         }
 
-        /**
-         * @see ca.bcit.infosys.timesheet.TimesheetCollection#addTimesheet()
-         * @return
-         */
-        @Override
-        public String addTimesheet() {
-            Timesheet t1 = new Timesheet();
-            t1.setEmployee(testEmployee);
-            currentTimesheet = t1;
-            timesheets.add(t1);
-            
-            return "createTimesheet";
+        return temp;
+    }
+
+    /**
+     * @see ca.bcit.infosys.timesheet.TimesheetCollection#addTimesheet()
+     * @return
+     */
+    @Override
+    public String addTimesheet() {
+        Timesheet t1 = new Timesheet();
+        ArrayList<TimesheetRow> rows = (ArrayList<TimesheetRow>) t1.getDetails();
+        for(int i = 0; i < 5;i++) {
+            rows.add(new TimesheetRow());
         }
+        t1.setEmployee(testEmployee);
+        currentTimesheet = t1;
+        timesheets.add(t1);
+
+        return "createTimesheet";
+    }
 
 
 }
